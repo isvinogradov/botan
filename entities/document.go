7aa3f0b4df6acabe930906863a273601cfe2fbae
Ivan Vinogradov <isvinogradov@yandex.ru>
2019-04-25 23:42:51 +0300

    f

--- entities/document.go
+++ entities/document.go
@@ -2,9 +2,9 @@ package entities
 
 // This object represents a general file (as opposed to photos, voice messages and audio files).
 type Document struct {
-	FileId   string    `json:"file_id"`             // Unique file identifier
-	Thumb    PhotoSize `json:"thumb,omitempty"`     // Optional. Document thumbnail as defined by sender
-	Filename string    `json:"file_name,omitempty"` // Optional. Original filename as defined by sender
-	MimeType string    `json:"mime_type,omitempty"` // Optional. MIME type of the file as defined by sender
-	FileSize int       `json:"file_size,omitempty"` // Optional. File size
+	FileId   string     `json:"file_id"`             // Unique file identifier
+	Thumb    *PhotoSize `json:"thumb,omitempty"`     // Optional. Document thumbnail as defined by sender
+	Filename string     `json:"file_name,omitempty"` // Optional. Original filename as defined by sender
+	MimeType string     `json:"mime_type,omitempty"` // Optional. MIME type of the file as defined by sender
+	FileSize int        `json:"file_size,omitempty"` // Optional. File size
 }
--- entities/game.go
+++ entities/game.go
@@ -7,5 +7,5 @@ type Game struct {
 	Photo        []PhotoSize     `json:"photo"`                   // Photo that will be displayed in the game message in chats.
 	Text         string          `json:"text,omitempty"`          // Optional. Brief description of the game or high scores included in the game message. Can be automatically edited to include current high scores for the game when the bot calls setGameScore, or manually edited using editMessageText. 0-4096 characters.
 	TextEntities []MessageEntity `json:"text_entities,omitempty"` // Optional. Special entities that appear in text, such as usernames, URLs, bot commands, etc.
-	Animation    Animation       `json:"animation,omitempty"`     // Optional. Animation that will be displayed in the game message in chats. Upload via BotFather
+	Animation    *Animation      `json:"animation,omitempty"`     // Optional. Animation that will be displayed in the game message in chats. Upload via BotFather
 }
