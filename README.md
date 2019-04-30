09de76230f3726bdda693220e5088f9ef3a47bde
IvanVinogradov@work <ivinogradov@ptsecurity.com>
2019-04-30 18:53:47 +0300

    f

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Tools for creating bots using Telegram Bot API
 ### TODO
 
   - add support for proxy in `transport` -> TEST IT
-  - send HTTP requests in goroutines
   - ChatId can be `int` or `string` (implement interface with `toString` method?) TEST IT
+  - send HTTP requests in goroutines
   - proper `InputFile` struct for files (some workaround with interfaces?)
   - check `chatAction`/`parseMode` enums
   - review transport method
--- config.go
+++ config.go
@@ -8,32 +8,3 @@ type Config struct {
 	GetUpdatesFailCooldownSeconds int    // sleep duration scheduled when getUpdates request fails
 	Socks5ConnectionString        string // connections string if SOCKS5 proxy is used
 }
-
-//func NewConfig(
-//	token string,
-//	postJsonTimeoutSec,
-//	longPollTimeoutSec,
-//	getUpdatesFailCooldownSec int,
-//	socks5ConnectionString string,
-//) (*Config, error) {
-//	if token == "" {
-//		return nil, errors.New("bot token not specified")
-//	}
-//	if postJsonTimeoutSec < 1 {
-//		postJsonTimeoutSec = 5
-//	}
-//	if longPollTimeoutSec < 1 {
-//		longPollTimeoutSec = 300
-//	}
-//	if getUpdatesFailCooldownSec < 1 {
-//		getUpdatesFailCooldownSec = 10
-//	}
-//
-//	return &Config{
-//		Token:                         token,
-//		PostJsonTimeoutSeconds:        postJsonTimeoutSec,
-//		LongPollTimeoutSeconds:        longPollTimeoutSec,
-//		GetUpdatesFailCooldownSeconds: getUpdatesFailCooldownSec,
-//		Socks5ConnectionString:        socks5ConnectionString,
-//	}, nil
-//}
